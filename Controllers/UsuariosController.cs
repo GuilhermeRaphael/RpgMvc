3bfcb2f1d4d5fd3d3a7797a03963c7e582c992e7
gabriel1001g <gabrielsg2000@gmail.com>
2025-06-11 20:11:25 -0300

    Aula 15 - Mvc

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RpgMvc.Models;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
 
 namespace RpgMvc.Controllers
 {
@@ -35,6 +39,39 @@ namespace RpgMvc.Controllers
         public IActionResult Error()
         {
             return View("Error!");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AutenticarAsync (UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Autenticar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar,content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    UsuarioViewModel uLogado = JsonConvert.DeserializeObject<UsuarioViewModel>(serialized);
+                    HttpContext.Session.SetString("SessionTokenUsuario", uLogado.Token);
+                    TempData["Mensagem"] = string.Format("Bem-vindo {0}!!!", uLogado.Username);
+                    return RedirectToAction("Index","Personagens");
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch(System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return View("IndexLogin");
+            }
         }
     }
 }
--- obj/Debug/net9.0/RpgMvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/RpgMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bffb2b92a07642ed28e21d7ab04f1dffbd5b5976")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
